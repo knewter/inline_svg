5b97dba58841d20c01b1777d2eb9b701af48e247
Josh Adams <josh.rubyist@gmail.com>
2016-07-02 01:40:30 -0600

    Handle either filename or pathname

--- lib/inline_svg/finds_asset_paths.rb
+++ lib/inline_svg/finds_asset_paths.rb
@@ -2,7 +2,11 @@ module InlineSvg
   class FindsAssetPaths
     def self.by_filename(filename)
       asset = configured_asset_finder.find_asset(filename)
-      asset && asset.filename
+      asset &&
+        (asset.respond_to?(:filename) ?
+          asset.filename :
+          asset.pathname
+        )
     end
 
     def self.configured_asset_finder
